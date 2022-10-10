b5efb9313e7647d9512f685c414f048b4b189847
odinokun <odinokun@gmail.com>
2022-10-10 11:55:39 +0200

    add mobile popup

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -12,6 +12,17 @@ module.exports = function () {
     $(targetCountry).addClass('active');
   })
 
+  $('.map-section__countries-link').on('click', function (e) {
+    e.preventDefault();
+
+    ClosePopup();
+
+    let targetPopup = '#country-popup-' + $(this).data('country');
+    $(targetPopup + ', #country-popup__layer').fadeIn();
+  })
+
+
+
   // begin popup close
   $('.country-popup__close').on('click', function () {
     ClosePopup();
